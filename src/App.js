32dd1c659ce7f012dcc98dfcc0b1d95b8338cfc3
Sara Sørensen <sarasorensen@Saras-MacBook-Air.local>
2021-02-11 10:52:45 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Layout from "./components/layout/Layout.jsx";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import "./style/sass/styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Layout></Layout>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <meta
+          name="description"
+          content="Discover our hotels, B&B's, guesthouses and more in Bergen!"
+        />
+        <title>Holidaze Hotel Booking</title>
+      </Helmet>
+      <div className="App">
+        <header className="App-header">
+          <Layout></Layout>
+        </header>
+      </div>
+    </HelmetProvider>
   );
 }
 
